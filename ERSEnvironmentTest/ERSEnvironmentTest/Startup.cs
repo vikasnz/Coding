28a0498df59cbefae50e908b7bc0d0d0c412570d
vikasnz <vikas_awasthi@outlook.com>
2018-08-25 21:42:36 +0000

    Update Startup.cs
    updated comments

--- ERSEnvironmentTest/ERSEnvironmentTest/Startup.cs
+++ ERSEnvironmentTest/ERSEnvironmentTest/Startup.cs
@@ -16,6 +16,7 @@ namespace ERSEnvironmentTest
     {
         public Startup(IConfiguration configuration)
         {
+            //add comments
             Configuration = configuration;
         }
 
