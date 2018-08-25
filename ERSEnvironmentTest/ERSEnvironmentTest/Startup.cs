446b40474bf799d8c0329bd51b8d38d7fde92396
vikasnz <vikas_awasthi@outlook.com>
2018-08-25 21:54:01 +0000

    Update Startup.cs
    added comment after rollback

--- ERSEnvironmentTest/ERSEnvironmentTest/Startup.cs
+++ ERSEnvironmentTest/ERSEnvironmentTest/Startup.cs
@@ -16,6 +16,7 @@ namespace ERSEnvironmentTest
     {
         public Startup(IConfiguration configuration)
         {
+            //GIT ROCKS!!!!!!!!!!!!!!!!!
             Configuration = configuration;
         }
 
